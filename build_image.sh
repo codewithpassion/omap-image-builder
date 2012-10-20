e913087f0f81ddd3260282486e155a045b3142b9
Dominik Fretz <info@codewithpassion.com>
2012-10-20 20:23:49 +0200

    adding picocom and arduino

--- build_image.sh
+++ build_image.sh
@@ -260,7 +260,7 @@ function precise_release {
 	reset_vars
 	DIST=precise
 	select_rcn-ee-net_kernel
-	EXTRA=",linux-firmware,devmem2,python-software-properties,gcc,g++,make,libjpeg-dev"
+	EXTRA=",linux-firmware,devmem2,python-software-properties,gcc,g++,make,libjpeg-dev,picocom,arduino"
 	FIXUPSCRIPT="fixup.sh"
 	MIRROR=$MIRROR_UBU
 	COMPONENTS="${UBU_COMPONENTS}"
